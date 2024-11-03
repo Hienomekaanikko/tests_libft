27b8bef5ee32501bd1c33c69cc46a5023ebe8712
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-11-03 10:59:48 +0200

    Update test_ft_strlen.c

--- test_ft_strlen.c
+++ test_ft_strlen.c
@@ -5,9 +5,7 @@
 //Strlen tests
 
 int main(void) {
-    // strlen
     printf("With input Hello, original strlen returns integer: %lu\n", strlen("Hello"));
     printf("With input Hello ft_strlen returns integer: %d\n", ft_strlen("Hello"));
-
     return 0;
 }
