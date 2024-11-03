8c1e357dc6632c06787877f1c7b5f5332bc48e13
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-11-03 11:13:21 +0200

    Add files via upload

--- test_ft_strlen.c
+++ test_ft_strlen.c
@@ -1,11 +1,8 @@
-#include "../Libft/libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-//Strlen tests
+#include "test_libft.h"
 
 int main(void) {
     printf("With input Hello, original strlen returns integer: %lu\n", strlen("Hello"));
     printf("With input Hello ft_strlen returns integer: %d\n", ft_strlen("Hello"));
+
     return 0;
 }
